99df263b7ffd07dcb2c96e24989e23e04c44a158
Davit Gogiava <ursamajor027@gmail.com>
2024-12-13 02:06:37 -0800

    Configure Mongoose in App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
-
+import { User, UserSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/chat-app'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
