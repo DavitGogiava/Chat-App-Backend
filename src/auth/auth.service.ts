06d18c01bfeb8685b9defe6a0986b6883a9ac5ad
Davit Gogiava <ursamajor027@gmail.com>
2024-12-13 02:01:04 -0800

    Update the Auth Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async register(registerUserDto: RegisterUserDto): Promise<User> {
+    const { username, password } = registerUserDto;
+    const existingUser = await this.userModel.findOne({ username }).exec();
+    if (existingUser) {
+      throw new Error('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new this.userModel({ username, password: hashedPassword });
+    return newUser.save();
+  }
+}
