184c0a900b4a46684ba0012690dba7747a752711
Davit Gogiava <ursamajor027@gmail.com>
2024-12-13 02:04:14 -0800

    Update the Auth Controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from '../schemas/user.schema';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
+    return this.authService.register(registerUserDto);
+  }
+}
